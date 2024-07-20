e310d617d1a82fa5bb13e2756c5981a4ac137f9e
DIVAM SHARMA <divamsharma21@gmail.com>
2024-07-20 14:56:44 +0530

    controller update

---
+++ controllers/Course.js
@@ -0,0 +1,131 @@
+const Course=require('../models/Course');
+const Tag=require('../models/tags');
+const User =require("../models/User");
+const {uploadImageToCloudinary}=require("../utils/imageUploader")
+
+
+
+
+// Create course handel function
+exports.createCourse= async(req,res)=>{
+try {
+    const { courseName, 
+        CourseDescription,
+        whatYouWillLearn,
+        price,
+        tag }=req.body;
+
+        // get thumbnail
+        const thumbnail =req.files.thumbnailImage;
+    
+        // validate
+        if(!courseName || !CourseDescription || !whatYouWillLearn || !price || !tag || !thumbnail){
+            return res.status(401).json({
+                success:false,
+                message:'All field are required ',
+            });
+        }
+
+        // check for instructor
+        const userId =req.user.id;
+        const instructorDetails =await User.findById(userId);
+        console.log("Instructor  details :",instructorDetails);
+
+        // Todo : verify that userid and instructordetails are same or not
+           if(userId != instructorDetails){
+            return res.status(401).json({
+                success:false,
+                message:'Instructor Details are  not equal to userid',
+            });
+           }
+
+           if(!instructorDetails){
+            return res.status(404).json({
+                success:false,
+                message:'Instructor Details not found',
+            });
+           }
+        // check given tag is vaild 
+          const tagDetails =await Tag.findById(tag);
+          if(!tagDetails){
+            return res.status(404).json({
+                success:false,
+                message:' Tags Details not found',
+            });
+          }
+        //upload image top Cloudinary
+            const thumbnailImage =await uploadImageToCloudinary(thumbnail,process.env.FOLDER_NAME);
+
+        // cretae an entry for new course
+           const newCourse=await Course.create({
+            courseName,
+            courseDescription,
+            instructor:instructorDetails._id,
+            whatYoutWillLearn: whatYoutWillLearn,
+            price,
+            tag : tagDetails._id,
+            thumbnail : thumbnailImage.secure_url,
+           })
+        // add the new course to the user schema of instructor
+            await User.findByIdAndUpdate(
+                {_id: instructorDetails._id},
+                {
+                    $push:{
+                        courses: newCourse._id,
+                    }
+                },
+                {new:true},
+            );
+        // update the tag schema 
+        // todo:HW
+        // it do by me
+        await Tag.findByIdAndUpdate(
+            {_id : tagDetails._id},
+            {
+                $push :{
+                    tags= newTags._id,
+                }
+            },
+            {new:true},
+        );
+
+        //return response
+        return res.status(200).json({
+            success:true,
+            message:"Course Created Successfully",
+            data:newCourse,
+        });
+} catch (error) {
+    console.error(error);
+    return res.status(500).json({
+        success:false,
+        message:'Failed to create Course',
+        error: error.message,
+    })
+}
+
+    
+};
+
+//getAllCourses handler function
+exports.showAllCourses = async (req, res) => {
+    try {
+            //TODO: change the below statement incrementally
+            const allCourses = await Course.find({});
+
+            return res.status(200).json({
+                success:true,
+                message:'Data for all courses fetched successfully',
+                data:allCourses,
+            })
+
+    }
+    catch(error) {
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message:'Cannot Fetch course data',
+            error:error.message,
+        })
+    }
+}
---
+++ controllers/Section.js
@@ -0,0 +1,13 @@
+const Section = require('../models/Section');
+const User = require('../muodels/User');
+
+// create section 
+exports.CreateSection = async(req,res)=>{
+    try {
+        //fetch data
+        // /validate
+        
+    } catch (err ) {
+        
+    }
+}
