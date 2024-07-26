2e24e74ba955c7548d7a47badfef4ba8335d6cb4
DIVAM SHARMA <divamsharma21@gmail.com>
2024-07-26 13:35:02 +0530

    update some controller

--- controllers/Course.js
+++ controllers/Course.js
@@ -1,6 +1,6 @@
 const Course=require('../models/Course');
 const Tag=require('../models/tags');
-const User =require("../models/User");
+const User =require("../models/Category");
 const {uploadImageToCloudinary}=require("../utils/imageUploader")
 
 
@@ -8,7 +8,7 @@ const {uploadImageToCloudinary}=require("../utils/imageUploader")
 
 // Create course handel function
 exports.createCourse= async(req,res)=>{
-try {
+ try {
     const { courseName, 
         CourseDescription,
         whatYouWillLearn,
@@ -95,7 +95,7 @@ try {
             message:"Course Created Successfully",
             data:newCourse,
         });
-} catch (error) {
+ } catch (error) {
     console.error(error);
     return res.status(500).json({
         success:false,
@@ -127,5 +127,55 @@ exports.showAllCourses = async (req, res) => {
             message:'Cannot Fetch course data',
             error:error.message,
         })
+    }
+}
+
+//getCourseDetails
+exports.getCourseDetails = async (req, res) => {
+    try {
+            //get id
+            const {courseId} = req.body;
+            //find course details
+            const courseDetails = await Course.find(
+                                        {_id:courseId})
+                                        .populate(
+                                            {
+                                                path:"instructor",
+                                                populate:{
+                                                    path:"additionalDetails",
+                                                },
+                                            }
+                                        )
+                                        .populate("category")
+                                        .populate("ratingAndreviews")
+                                        .populate({
+                                            path:"courseContent",
+                                            populate:{
+                                                path:"subSection",
+                                            },
+                                        })
+                                        .exec();
+
+                //validation
+                if(!courseDetails) {
+                    return res.status(400).json({
+                        success:false,
+                        message:`Could not find the course with ${courseId}`,
+                    });
+                }
+                //return response
+                return res.status(200).json({
+                    success:true,
+                    message:"Course Details fetched successfully",
+                    data:courseDetails,
+                })
+
+    }
+    catch(error) {
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message:error.message,
+        });
     }
 }
